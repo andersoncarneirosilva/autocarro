84b58e3b859d2a3158f8e4abfda6661674bc7b8f
andersonseidler <andersonqipoa@gmail.com>
2024-12-24 09:39:18 -0300

    Site finalizado

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -513,7 +513,7 @@
     <footer class="bg-dark py-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <img src="assets/images/logo.png" alt="logo" class="logo-dark" height="22" />
                     <p class="text-light text-opacity-50 mt-4">Gestão Simplificada e Controle Total em um só lugar.
                     </p>
@@ -535,7 +535,7 @@
 
                 </div>
 
-                <div class="col-lg-2 col-md-4 mt-3 mt-lg-0">
+                {{-- <div class="col-lg-2 col-md-4 mt-3 mt-lg-0">
                     <h5 class="text-light">Company</h5>
 
                     <ul class="list-unstyled ps-0 mb-0 mt-3">
@@ -568,7 +568,7 @@
                         <li class="mt-2"><a href="javascript: void(0);" class="text-light text-opacity-50">Privacy</a></li>
                     </ul>
                 </div>
-            </div>
+            </div> --}}
 
             <div class="row">
                 <div class="col-lg-12">
