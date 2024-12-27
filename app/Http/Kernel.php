560793f7a1c580eb99b17ef8a2c7f1c271462aaa
andersonseidler <andersonqipoa@gmail.com>
2024-12-26 23:48:01 -0300

    Att: kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -74,6 +74,7 @@ class Kernel extends HttpKernel
         // Outros middlewares
         'uppercase' => \App\Http\Middleware\UppercaseInput::class,
         //'tenant.database' => \App\Http\Middleware\InitializeTenantDatabase::class,
+        'tenant.domain' => \App\Http\Middleware\TenantDomainMiddleware::class,
     ];
     
 }
---
+++ app/Http/Middleware/TenantDomainMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class TenantDomainMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Obtém o domínio/subdomínio do host
+        $tenantDomain = $request->getHost();
+
+        // Recupera a lista de domínios da configuração
+        $centralDomains = config('central_domains.central_domains');
+
+        // Verifica se o domínio está na lista
+        if (!in_array($tenantDomain, $centralDomains)) {
+            // Se o domínio não estiver na lista, aborta e retorna um erro
+            abort(404, 'Tenant could not be identified on domain ' . $tenantDomain);
+        }
+
+        return $next($request); // Continua a execução da requisição
+    }
+}
