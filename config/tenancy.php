8d188d3e866ed32056711866d1b9bdb43c1110c4
andersonseidler <andersonqipoa@gmail.com>
2024-12-26 23:35:48 -0300

    Att: central_domains

--- config/tenancy.php
+++ config/tenancy.php
@@ -19,8 +19,8 @@ return [
      */
     'central_domains' => [
         '143.1140.148.157',
-        'webprosites.com.br'
-        '*.webprosites.com.br'
+        'webprosites.com.br',
+        '*.webprosites.com.br',
     ],
 
     
