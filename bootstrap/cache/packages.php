b14ebf468c5663d77e483bc47bd63f6450c773e7
andersonseidler <andersonqipoa@gmail.com>
2024-12-30 11:32:05 -0300

    composer

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,11 +1,4 @@
 <?php return array (
-  'akaunting/laravel-money' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Akaunting\\Money\\Provider',
-    ),
-  ),
   'barryvdh/laravel-dompdf' => 
   array (
     'aliases' => 
@@ -16,55 +9,6 @@
     'providers' => 
     array (
       0 => 'Barryvdh\\DomPDF\\ServiceProvider',
-    ),
-  ),
-  'blade-ui-kit/blade-heroicons' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'BladeUI\\Heroicons\\BladeHeroiconsServiceProvider',
-    ),
-  ),
-  'blade-ui-kit/blade-icons' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'BladeUI\\Icons\\BladeIconsServiceProvider',
-    ),
-  ),
-  'filament/filament' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Filament\\FilamentServiceProvider',
-    ),
-  ),
-  'filament/forms' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Filament\\Forms\\FormsServiceProvider',
-    ),
-  ),
-  'filament/notifications' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Filament\\Notifications\\NotificationsServiceProvider',
-    ),
-  ),
-  'filament/support' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Filament\\Support\\SupportServiceProvider',
-    ),
-  ),
-  'filament/tables' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Filament\\Tables\\TablesServiceProvider',
     ),
   ),
   'intervention/image' => 
@@ -106,17 +50,6 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
-  'livewire/livewire' => 
-  array (
-    'aliases' => 
-    array (
-      'Livewire' => 'Livewire\\Livewire',
-    ),
-    'providers' => 
-    array (
-      0 => 'Livewire\\LivewireServiceProvider',
-    ),
-  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
@@ -154,17 +87,6 @@
     'providers' => 
     array (
       0 => 'RealRashid\\SweetAlert\\SweetAlertServiceProvider',
-    ),
-  ),
-  'ryangjchandler/blade-capture-directive' => 
-  array (
-    'aliases' => 
-    array (
-      'BladeCaptureDirective' => 'RyanChandler\\BladeCaptureDirective\\Facades\\BladeCaptureDirective',
-    ),
-    'providers' => 
-    array (
-      0 => 'RyanChandler\\BladeCaptureDirective\\BladeCaptureDirectiveServiceProvider',
     ),
   ),
   'spatie/laravel-ignition' => 
