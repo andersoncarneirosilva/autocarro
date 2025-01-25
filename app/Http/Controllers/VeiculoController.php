f50ee0d5fc39f69d14a979f2ef4b8c699b6e915f
andersonseidler <andersonqipoa@gmail.com>
2025-01-25 13:51:53 -0300

    update atpve4

--- app/Http/Controllers/VeiculoController.php
+++ app/Http/Controllers/VeiculoController.php
@@ -645,7 +645,7 @@ if ($this->model->create($data)) {
         $clienteId = $request->input('cliente');
         //dd($clienteIds);
         $cliente = Cliente::whereIn('id', $clienteId)->first();
-        dd($cliente->nome);
+        //dd($cliente->nome);
         // foreach ($clientes as $cliente) {
         // }
         
