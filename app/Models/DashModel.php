36f3fcc6fc0f581b4dab1d013c486a596e6200f9
andersonseidler <andersonqipoa@gmail.com>
2025-01-26 00:49:58 -0300

    update count docs

--- app/Models/DashModel.php
+++ app/Models/DashModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use App\Models\Adiantamento;
 use Carbon\Carbon;
-
+use Illuminate\Support\Facades\Auth;
 class DashModel extends Model
 {
     use HasFactory;
@@ -21,14 +21,19 @@ class DashModel extends Model
 
     public function getCountDocs(){
         $counts = [];
+        $userId = Auth::id(); // Obtém o ID do usuário logado
+    
         for ($month = 1; $month <= 12; $month++) {
             $countMonth = DB::table('veiculos')
                             ->whereMonth('created_at', $month)
+                            ->where('user_id', $userId) // Filtra pelo ID do usuário
                             ->count();
             $counts[] = $countMonth;
         }
+    
         return $counts;
     }
+    
     
 
     public function getCountProcs(){
