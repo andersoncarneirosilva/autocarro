0d5202dfc5a5ec30aae859a2936ef4759b4d56ed
andersonseidler <andersonqipoa@gmail.com>
2025-01-27 20:11:06 -0300

    Update login2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         Event::observe(EventObserver::class);
 
         // if (config('app.env') !== 'local') {
