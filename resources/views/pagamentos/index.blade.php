59d6d4187bdada46a5ab6b4d524caf4a8b5efe32
andersonseidler <andersonqipoa@gmail.com>
2025-02-09 17:01:13 -0300

    Teste pagamentos

--- resources/views/pagamentos/index.blade.php
+++ resources/views/pagamentos/index.blade.php
@@ -21,121 +21,127 @@
 <div id="paymentBrick_container">
 </div>
 <script>
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    const mp = new MercadoPago('TEST-83c1af18-f3a5-4077-bc98-e72379b980b1', {
+      locale: 'pt'
+    });
+    const bricksBuilder = mp.bricks();
   
-  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-const mp = new MercadoPago('TEST-83c1af18-f3a5-4077-bc98-e72379b980b1', {
-  locale: 'pt'
-});
-const bricksBuilder = mp.bricks();
-  const renderPaymentBrick = async (bricksBuilder) => {
-    // Requisição para obter um preferenceId do backend
-    const preferenceResponse = await fetch('/api/create-preference', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': csrfToken
-            },
-            body: JSON.stringify({
-                amount: 10000,
-                payer_email: "andersonqipoa@gmail.com"
-            })
+    const renderPaymentBrick = async () => {
+      try {
+        // Requisição para obter um preferenceId do backend
+        const preferenceResponse = await fetch('/api/create-preference', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken
+          },
+          body: JSON.stringify({
+            amount: 10000,
+            payer_email: "andersonqipoa@gmail.com"
+          })
         });
-
+  
         const preferenceData = await preferenceResponse.json();
         if (!preferenceData.preferenceId) {
-            console.error("Erro ao obter preferenceId:", preferenceData);
-            alert("Erro ao carregar o pagamento.");
-            return;
+          console.error("Erro ao obter preferenceId:", preferenceData);
+          alert("Erro ao carregar o pagamento.");
+          return;
         }
-    const settings = {
-      initialization: {
-        /*
-          "amount" é a quantia total a pagar por todos os meios de pagamento com exceção da Conta Mercado Pago e Parcelas sem cartão de crédito, que têm seus valores de processamento determinados no backend através do "preferenceId"
-        */
-        amount: 10000,
-        preferenceId: "<PREFERENCE_ID>",
-        payer: {
-          firstName: "Anderson",
-          lastName: "Carneiro",
-          email: "andersonqipoa@gmail.com",
-        },
-      },
-      customization: {
-        visual: {
-          style: {
-            theme: "default",
+  
+        const settings = {
+          initialization: {
+            amount: 10000,
+            preferenceId: preferenceData.preferenceId,  // Use o preferenceId retornado aqui
+            payer: {
+              firstName: "Anderson",
+              lastName: "Carneiro",
+              email: "andersonqipoa@gmail.com",
+            },
           },
-        },
-        paymentMethods: {
-            creditCard: "all",
-            atm: "all",
-            bankTransfer: "all",
-            debitCard: "all",
-			ticket: "all",
-          maxInstallments: 1
-        },
-      },
-      callbacks: {
-        onReady: () => {
-          /*
-           Callback chamado quando o Brick está pronto.
-           Aqui, você pode ocultar seu site, por exemplo.
-          */
-        },
-        
-
-        onSubmit: ({ selectedPaymentMethod, formData }) => {
-            return new Promise((resolve, reject) => {
+          customization: {
+            visual: {
+              style: {
+                theme: "default",
+              },
+            },
+            paymentMethods: {
+              creditCard: "all",
+              atm: "all",
+              bankTransfer: "all",
+              debitCard: "all",
+              ticket: "all",
+              maxInstallments: 1
+            },
+          },
+          callbacks: {
+            onReady: () => {
+              /*
+               Callback chamado quando o Brick está pronto.
+               Aqui, você pode ocultar seu site, por exemplo.
+              */
+            },
+  
+            onSubmit: ({ selectedPaymentMethod, formData }) => {
+              return new Promise((resolve, reject) => {
                 // Verificar se o paymentMethodId está presente
                 if (!selectedPaymentMethod) {
-                    alert('Por favor, selecione um método de pagamento.');
-                    reject();
-                    return;
+                  alert('Por favor, selecione um método de pagamento.');
+                  reject();
+                  return;
                 }
                 formData.paymentMethodId = selectedPaymentMethod.id;
-
+  
                 // Enviar a requisição
                 fetch("/api/payment-updated", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "X-CSRF-TOKEN": csrfToken, // Adiciona o token CSRF nos cabeçalhos
-                    },
-                    body: JSON.stringify(formData),
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": csrfToken, // Adiciona o token CSRF nos cabeçalhos
+                  },
+                  body: JSON.stringify(formData),
                 })
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log('Resposta do servidor:', data);  // Verifique o conteúdo da resposta
+                  .then((response) => response.json())
+                  .then((data) => {
+                    console.log('Resposta do servidor no script:', data);  // Verifique o conteúdo da resposta
                     if (data.status === 'success') {
-                        alert(data.message);  // Exibe a mensagem de sucesso
-                        resolve();  // Indica que o pagamento foi processado com sucesso
+                      alert(data.message);  // Exibe a mensagem de sucesso
+                      resolve();  // Indica que o pagamento foi processado com sucesso
                     } else {
-                        alert('Erro no pagamento: ' + data.message);  // Exibe a mensagem de erro
-                        reject();  // Indica que houve um erro no pagamento
+                      alert('Erro no pagamento: ' + data.message);  // Exibe a mensagem de erro
+                      reject();  // Indica que houve um erro no pagamento
                     }
-                })
-                .catch((error) => {
+                  })
+                  .catch((error) => {
                     console.error('Erro na requisição:', error);
                     alert('Erro na comunicação com o servidor.');
                     reject();  // Caso ocorra erro na requisição
-                });
-
-            });
-        },
-
-        onError: (error) => {
-          // callback chamado para todos os casos de erro do Brick
-          console.error(error);
-        },
-      },
+                  });
+              });
+            },
+  
+            onError: (error) => {
+              // callback chamado para todos os casos de erro do Brick
+              console.error(error);
+            },
+          },
+        };
+  
+        // Criando o Brick de pagamento
+        window.paymentBrickController = await bricksBuilder.create(
+          "payment",
+          "paymentBrick_container",
+          settings
+        );
+  
+      } catch (error) {
+        console.error("Erro ao renderizar Brick:", error);
+        alert("Erro ao carregar pagamento.");
+      }
     };
-    window.paymentBrickController = await bricksBuilder.create(
-      "payment",
-      "paymentBrick_container",
-      settings
-    );
-  };
-  renderPaymentBrick(bricksBuilder);
-</script>
+  
+    renderPaymentBrick();
+  </script>
+  
 
 @endsection
