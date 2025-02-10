7c830d4f0f615df6d93351ccdd21bfb189444fd3
andersonseidler <andersonqipoa@gmail.com>
2025-02-09 23:10:22 -0300

    Teste webohook

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\PaymentController;
 |
 */
 //Route::get('/payment', [PaymentController::class, 'index'])->name('payment.index');
-Route::post('/payment-updated', [PaymentController::class, 'webhook'])->name('payment.updated');
+//Route::post('/payment-updated', [PaymentController::class, 'webhook'])->name('payment.updated');
 Route::post('/create-preference', [PaymentController::class, 'createPreference']);
 
 Route::post('/payment-updated', [PaymentController::class, 'handleWebhook']);
