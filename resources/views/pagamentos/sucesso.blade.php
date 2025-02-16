fbdbce263f95dd092c937549e6d355de8262cfd7
andersonseidler <andersonqipoa@gmail.com>
2025-02-16 09:17:30 -0300

    Update pedidos

--- resources/views/pagamentos/sucesso.blade.php
+++ resources/views/pagamentos/sucesso.blade.php
@@ -9,16 +9,25 @@
         <div class="page-title-box">
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item active">Dashboard</li>
                 </ol>
             </div>
-            <h3 class="page-title">Dashboard</h3>
         </div>
     </div>
 </div>
 <br>
 
-<h1>Pagamento Realizado com Sucesso!</h1>
-    <p>Obrigado pelo seu pagamento. Seu pedido foi processado com sucesso.</p>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="text-center">
+            <h3 class="">Pagamento realizado com sucesso! <i class="mdi mdi-check text-success"></i></h3>
+            <p class="text-muted mt-3">O crédito foi adicionado a sua conta.</p>
+
+            <a href="{{ route('dashboard.index') }}" class="btn btn-success btn-sm mt-2">
+                <i class="mdi mdi-view-dashboard-outline me-1"></i> Ir para Dashboard
+            </a>
+        </div>
+    </div><!-- end col -->
+</div>
+
 
 @endsection
