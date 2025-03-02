8333f3e96c72e7aac40ee1f68b6913061c11bfd3
root <root@DESKTOP-T9S5AC0>
2025-03-02 13:07:47 -0300

    Upload video gerar atpve

--- resources/views/ajuda/index.blade.php
+++ resources/views/ajuda/index.blade.php
@@ -84,43 +84,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="card mb-0">
-                    <div class="card-header" id="headingSix">
-                        <h5 class="m-0">
-                            <a class="custom-accordion-title collapsed d-block"
-                                data-bs-toggle="collapse" href="#collapseSix"
-                                aria-expanded="false" aria-controls="collapseSix">
-                                Q. How do I get help with the theme? <i
-                                    class="mdi mdi-chevron-down accordion-arrow"></i>
-                            </a>
-                        </h5>
-                    </div>
-                    <div id="collapseSix" class="collapse" aria-labelledby="headingSix"
-                        data-bs-parent="#custom-accordion-one">
-                        <div class="card-body">
-                            ...
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-0">
-                    <div class="card-header" id="headingSeven">
-                        <h5 class="m-0">
-                            <a class="custom-accordion-title collapsed d-block"
-                                data-bs-toggle="collapse" href="#collapseSeven"
-                                aria-expanded="false" aria-controls="collapseSeven">
-                                Q. Will you regularly give updates of Hyper ? <i
-                                    class="mdi mdi-chevron-down accordion-arrow"></i>
-                            </a>
-                        </h5>
-                    </div>
-                    <div id="collapseSeven" class="collapse"
-                        aria-labelledby="headingSeven"
-                        data-bs-parent="#custom-accordion-one">
-                        <div class="card-body">
-                            ...
-                        </div>
-                    </div>
-                </div>
+
+                
             </div>
         </div>
     </div>
