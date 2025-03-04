013c8a0164b2e1fb228d5ba27d07485a7af09e33
andersonseidler <andersonqipoa@gmail.com>
2025-03-03 22:44:22 -0300

    Update alert email

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="shortcut icon" href="{{ url('images/favicon.ico') }}">
+    <script src="{{ url('assets/js/notificacoes.js') }}?v={{ time() }}"></script>
     <!-- Daterangepicker css -->
     <link href="{{ url('assets/vendor/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="{{ url('assets/vendor/daterangepicker/daterangepicker.css') }}">
@@ -62,7 +63,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-    <script src="{{ url('assets/js/notificacoes.js') }}?v={{ time() }}"></script>
     <script src="https://sdk.mercadopago.com/js/v2"></script>
     <script>
         $(document).ready(function() {
