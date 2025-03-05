b50c32f905d443bcec603f086e10d27d1abe975d
andersonseidler <andersonqipoa@gmail.com>
2025-03-05 11:32:23 -0300

    Correcao footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
       <div class="row">
           <div class="col-md-6">
-              <script>document.write(new Date().getFullYear())</script> © Procuração Online - proconline.com.br
+            2024 - <script>document.write(new Date().getFullYear())</script> © Procuração Online - proconline.com.br
           </div>
       </div>
   </div>
