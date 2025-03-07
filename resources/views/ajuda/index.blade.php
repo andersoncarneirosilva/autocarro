f785118541c35d5ab3b299a7da69249fe5b6e39e
andersonseidler <andersonqipoa@gmail.com>
2025-03-07 19:45:00 -0300

    Update ajuda

--- resources/views/ajuda/index.blade.php
+++ resources/views/ajuda/index.blade.php
@@ -27,12 +27,12 @@
             <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                            aria-expanded="true" aria-controls="collapseOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                            aria-expanded="false" aria-controls="collapseOne">
                             Como cadastrar um veículo?
                         </button>
                     </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
                         data-bs-parent="#accordionExample">
                         <div class="accordion-body">
                             <h5>Cadastrar o veículo e baixar a procuração</h5>
@@ -42,7 +42,7 @@
                             </ul>
                             <div class="mb-3">
                                 <video controls class="w-100" style="max-width: 640px; height: auto;">
-                                    <source src="https://proconline.com.br/cadastrar-veiculo.mp4" type="video/mp4">
+                                    <source src="https://proconline.com.br/images/videos/cadastrar-veiculo.mp4" type="video/mp4">
                                     Seu navegador não suporta a tag de vídeo.
                                 </video>
                             </div>
@@ -61,12 +61,13 @@
                         <div class="accordion-body">
                             <h5>Cadastrar o veículo e baixar a procuração</h5>
                             <ul>
-                                <li>Clique no botão cadastrar e selecione o cadastro automático.</li>
-                                <li>Insira o endereço do outorgado e o documento em PDF.</li>
+                                <li>Acesse a página <strong>veículos</strong>, clique no menu <strong>Ações</strong>.</li>
+                                <li>Em seguida clique em <strong>Gerar solicitação ATPVe</strong>.</li>
+                                <li>Selecione o <strong>Outorgado</strong>, <strong>cliente</strong> e preencha o valor da venda.</li>
                             </ul>
                             <div class="mb-3">
                                 <video controls class="w-100" style="max-width: 640px; height: auto;">
-                                    <source src="https://proconline.com.br/gerar-atpve.mp4" type="video/mp4">
+                                    <source src="https://proconline.com.br/images/videos/gerar-atpve.mp4" type="video/mp4">
                                     Seu navegador não suporta a tag de vídeo.
                                 </video>
                             </div>
@@ -79,4 +80,5 @@
         </div>
     </div>
 </div>
+
 @endsection
