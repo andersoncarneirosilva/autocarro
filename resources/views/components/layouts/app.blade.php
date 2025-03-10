1bbe0ca0fc0ab0d6e8d5d8fb3dcd15146a9d3dcc
andersonseidler <andersonqipoa@gmail.com>
2025-03-10 20:17:04 -0300

    Update vite

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -60,14 +60,16 @@
 <link href="{{ url('assets/vendor/jstree/themes/default/style.min.css') }}" rel="stylesheet" type="text/css">
 
 @if (app()->environment('local'))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        @php
-            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-        @endphp
-        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
-        <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
-    @endif
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+@php
+    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    $cssFile = $manifest['resources/css/app.css']['file'] ?? 'assets/app.css';  // Default case
+    $jsFile = $manifest['resources/js/app.js']['file'] ?? 'assets/app.js';  // Default case
+@endphp
+<link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
+<script src="{{ asset('build/' . $jsFile) }}" defer></script>
+@endif
                         
 
 
