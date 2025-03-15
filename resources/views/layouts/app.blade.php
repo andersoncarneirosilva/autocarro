52c5b759cb91939e4115ad358e690b5026ad8d88
andersonseidler <andersonqipoa@gmail.com>
2025-03-15 15:00:53 -0300

    chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,21 @@
 <link href="{{ url('assets/vendor/jstree/themes/default/style.min.css') }}" rel="stylesheet" type="text/css">
 
                                                
-                                                    
+@if (app()->environment('local'))
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+@php
+    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    $cssFile = $manifest['resources/css/app.css']['file'] ?? 'assets/app.css';  // Default case
+    $jsFile = $manifest['resources/js/app.js']['file'] ?? 'assets/app.js';  // Default case
+@endphp
+<link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
+<script src="{{ asset('build/' . $jsFile) }}" defer></script>
+@endif
+                
+<script>
+var userId = @json(auth()->id());
+</script>                                        
 
 
     <!-- Plugin css -->
