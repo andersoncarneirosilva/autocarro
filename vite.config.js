d507821e4f51cbe8560578db24edd52c2c653688
andersonseidler <andersonqipoa@gmail.com>
2025-03-14 23:12:54 -0300

    Update bootstrap.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,13 @@ import laravel from 'laravel-vite-plugin';
 
 // Configure apenas em ambiente local
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    server: {
-        // Só ativa o servidor do Vite em ambiente de desenvolvimento
-        host: process.env.APP_ENV === 'local' ? 'localhost' : false, 
-        port: 5173,
-        strictPort: true,
+    define: {
+        'process.env': {
+            VITE_PUSHER_APP_KEY: process.env.VITE_PUSHER_APP_KEY,
+            VITE_PUSHER_HOST: process.env.VITE_PUSHER_HOST,
+            VITE_PUSHER_PORT: process.env.VITE_PUSHER_PORT,
+            VITE_PUSHER_SCHEME: process.env.VITE_PUSHER_SCHEME,
+            VITE_PUSHER_APP_CLUSTER: process.env.VITE_PUSHER_APP_CLUSTER,
+        },
     },
 });
