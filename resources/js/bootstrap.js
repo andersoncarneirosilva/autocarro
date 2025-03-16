928b57f9fb94265456958e133d5cd9a65518c52c
andersonseidler <andersonqipoa@gmail.com>
2025-03-16 18:02:34 -0300

    chat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,35 @@
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
+
+// window.Pusher = Pusher;
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//     wsHost: import.meta.env.VITE_PUSHER_HOST,
+//     wsPort: import.meta.env.VITE_PUSHER_PORT,
+//     wssPort: null,  // Não usar o protocolo wss em ambientes não SSL
+//     forceTLS: false,  // Garantir que o TLS (HTTPS) não seja utilizado, caso esteja usando http
+//     enabledTransports: ['ws'],  // Usar somente WebSocket não seguro (para desenvolvimento local)
+//     disableStats: true,
+//     authEndpoint: '/broadcasting/auth',
+//     auth: {
+//         headers: {
+//             'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content,
+//         },
+//     },
+// });
+
+// console.log("Instância de Echo criada com sucesso!");
+
+// window.Echo.connector.pusher.connection.bind('state_change', function(states) {
+//     console.log("Estado da conexão Pusher:", states);
+//     if (states.current === 'connected') {
+//         console.log("Conexão Pusher estabelecida com sucesso!");
+//     }
+// });
+
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
@@ -8,12 +40,10 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    wssPort: null,  // Não usar o protocolo wss em ambientes não SSL
-    //forceTLS: false,
-    forceTLS: true, // PARA PRODUCAO
-    // enabledTransports: ['ws'],  // Usar somente WebSocket não seguro (para desenvolvimento local)
-    enabledTransports: ['wss'],  //PARA PRODUCAO
+    wsPort: import.meta.env.VITE_PUSHER_PORT,  // Usando a porta 6001 para WebSockets
+    wssPort: 6001,  // Usar a mesma porta 6001 para wss (WebSocket seguro)
+    forceTLS: true,  // Forçar o uso de TLS em produção
+    enabledTransports: ['ws', 'wss'],  // Habilitar ws (não seguro) e wss (seguro)
     disableStats: true,
     authEndpoint: '/broadcasting/auth',
     auth: {
@@ -31,23 +61,3 @@ window.Echo.connector.pusher.connection.bind('state_change', function(states) {
         console.log("Conexão Pusher estabelecida com sucesso!");
     }
 });
-
-
-
-
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
-
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: window.location.hostname,
-//     wsPort: 6001,
-//     wssPort: 6001,
-//     forceTLS: false,
-//     disableStats: true,
-//     enabledTransports: ['ws'],
-// });
