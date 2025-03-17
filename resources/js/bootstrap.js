a6c7fcd404fbb69d29e524793f0a0f7323c1f825
andersonseidler <andersonqipoa@gmail.com>
2025-03-16 22:47:54 -0300

    chat boostrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -44,8 +44,6 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: true,
     authEndpoint: '/broadcasting/auth',
     auth: {
