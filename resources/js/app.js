7f4303bf52476fcd2abcd42eb17ca7ee46472625
andersonseidler <andersonqipoa@gmail.com>
2025-03-17 21:38:37 -0300

    chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,24 @@
 import './bootstrap';
 import './test';
-
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("Iniciando escuta no canal 'chat.{userId}'...");
-
+    console.log("Iniciando escuta no canal 'app.js'...");
+    console.log("authUserId:", window.authUserId);
     const messageList = document.getElementById('message-list');
-    const userId = window.authUserId;  // Certifique-se de que `authUserId` está disponível
 
     if (messageList) {
-        // Assinando o canal privado 'chat.{userId}'
-        window.Echo.private('chat.' + userId)
+        window.Echo.channel('chat')
             .listen('NewMessage', (event) => {
                 try {
-                    console.log('Nova mensagem recebida:', event);
+                    console.log('Nova mensagem recebida:', event); // Verifique os dados recebidos no console
 
-                    if (!event.content) {
+                    if (!event.id || !event.content || !event.sender_id || !event.created_at) {
                         console.error('Dados de mensagem inválidos:', event);
                         return;
                     }
 
                     const newMessage = document.createElement('li');
-                    const senderClass = event.sender_id === userId ? 'user-message' : 'admin-message';
+                    const senderClass = event.sender_id === window.authUserId ? 'user-message' : 'admin-message';
                     newMessage.classList.add(senderClass);
-
                     // Formatar a hora com 'H:i'
                     const createdAt = new Date(event.created_at);
                     const formattedTime = createdAt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
