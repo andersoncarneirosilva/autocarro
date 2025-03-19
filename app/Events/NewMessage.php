a14045e26e976cc5705700c27c10e248a346f8f3
andersonseidler <andersonqipoa@gmail.com>
2025-03-19 20:47:05 -0300

    chat

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -2,47 +2,18 @@
 
 namespace App\Events;
 
-use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-use Log;
 
-class NewMessage implements ShouldBroadcast
+class SimpleTestEvent implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public $message;
-
-    public function __construct(Message $message)
-    {
-        $this->message = $message;
-        Log::info('✅ Evento NewMessage CONSTRUTOR chamado!', ['message' => $message->content]);
-    }
-
     public function broadcastOn()
     {
-        Log::info('📡 Transmitindo no canal chat');
-        return new Channel('chat'); // Garantir que o canal é público
+        return new Channel('chat');
     }
 
     public function broadcastAs()
     {
-        Log::info('📢 Nome do evento: NewMessage');
-        return 'NewMessage';
-    }
-
-    public function broadcastWith()
-    {
-        Log::info('📤 Dados enviados no evento:', ['message' => $this->message]);
-
-        return [
-            'id' => $this->message->id,
-            'content' => $this->message->content,
-            'sender_id' => $this->message->sender_id,
-            'created_at' => $this->message->created_at->toISOString(),
-        ];
+        return 'SimpleTestEvent';
     }
 }
