c138b41a7b897735bf71a041f3c8b5a8626b000b
andersonseidler <andersonqipoa@gmail.com>
2025-03-18 21:03:20 -0300

    chat

--- routes/web.php
+++ routes/web.php
@@ -47,21 +47,21 @@ Route::get('/test-event', function () {
 
     return 'Evento disparado!';
 });
-// Route::post('/broadcasting/auth', function (Request $request) {
-//     Log::info('Recebendo autenticação WebSocket', [
-//         'user_id' => auth()->id(),
-//         'socket_id' => $request->socket_id
-//     ]);
+Route::post('/broadcasting/auth', function (Request $request) {
+    Log::info('Recebendo autenticação WebSocket', [
+        'user_id' => auth()->id(),
+        'socket_id' => $request->socket_id
+    ]);
 
-//     // Verifica se o usuário está autenticado
-//     if (!auth()->check()) {
-//         Log::error('Usuário não autenticado.');
-//         return response()->json(['error' => 'Usuário não autenticado'], 403);
-//     }
+    // Verifica se o usuário está autenticado
+    if (!auth()->check()) {
+        Log::error('Usuário não autenticado.');
+        return response()->json(['error' => 'Usuário não autenticado'], 403);
+    }
 
-//     // Retorna a resposta de sucesso para o Pusher
-//     return response()->json(['message' => 'Autenticado']);
-// });
+    // Retorna a resposta de sucesso para o Pusher
+    return response()->json(['message' => 'Autenticado']);
+});
 
     Route::get('/chat', \App\Livewire\Chat::class)->name('chat');
 
