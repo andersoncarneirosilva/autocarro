eb2b7eb19a360cd4a1ae7219480498a042ed20ff
andersonseidler <andersonqipoa@gmail.com>
2025-03-19 19:48:59 -0300

    chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,13 +6,34 @@ document.addEventListener('DOMContentLoaded', function () {
     const messageList = document.getElementById('message-list');
 
     if (messageList) {
-        var channel = pusher.subscribe('chat');
+        window.Echo.channel('chat')
+    .listen('.NewMessage', (event) => {
+        console.log('Nova mensagem recebida:', event); // Verifique o evento completo
 
-        channel.bind('my-event', function(data) {
-        
-          alert('Received my-event with message: ' + data.message);
-        
-        });
+        if (event && event.content && event.sender_id && event.created_at) {
+            const newMessage = document.createElement('li');
+            const senderClass = event.sender_id === window.authUserId ? 'user-message' : 'admin-message';
+            newMessage.classList.add(senderClass);
+
+            // Formatar a hora com 'H:i'
+            const createdAt = new Date(event.created_at);
+            const formattedTime = createdAt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+            newMessage.innerHTML = `
+                <div class="conversation-text">
+                    <div class="ctext-wrap">
+                        <p>${event.content}</p>
+                    </div>
+                    <span class="message-time">${formattedTime}</span>
+                </div>
+            `;
+
+            messageList.appendChild(newMessage);
+            messageList.scrollTop = messageList.scrollHeight;
+        } else {
+            console.error('Mensagem inválida recebida:', event);
+        }
+    });
     } else {
         console.warn("Elemento #message-list não encontrado.");
     }
