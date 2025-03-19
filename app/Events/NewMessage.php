ce73b9212f9a70b9dfcd6deb73a131ebed14617b
andersonseidler <andersonqipoa@gmail.com>
2025-03-19 20:47:37 -0300

    chat

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -2,18 +2,47 @@
 
 namespace App\Events;
 
+use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Log;
 
-class SimpleTestEvent implements ShouldBroadcast
+class NewMessage implements ShouldBroadcast
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+        Log::info('✅ Evento NewMessage CONSTRUTOR chamado!', ['message' => $message->content]);
+    }
+
     public function broadcastOn()
     {
-        return new Channel('chat');
+        Log::info('📡 Transmitindo no canal chat');
+        return new Channel('chat'); // Garantir que o canal é público
     }
 
     public function broadcastAs()
     {
-        return 'SimpleTestEvent';
+        Log::info('📢 Nome do evento: NewMessage');
+        return 'NewMessage';
+    }
+
+    public function broadcastWith()
+    {
+        Log::info('📤 Dados enviados no evento:', ['message' => $this->message]);
+
+        return [
+            'id' => $this->message->id,
+            'content' => $this->message->content,
+            'sender_id' => $this->message->sender_id,
+            'created_at' => $this->message->created_at->toISOString(),
+        ];
     }
 }
--- app/Events/SimpleTestEvent.php
+++ app/Events/SimpleTestEvent.php
@@ -3,26 +3,17 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-use Log;
 
 class SimpleTestEvent implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public $message;
-
-    public function __construct()
-    {
-        $this->message = "Hello from Laravel!";
-        Log::info("SimpleTestEvent triggered!");
-    }
-
     public function broadcastOn()
     {
         return new Channel('chat');
+    }
+
+    public function broadcastAs()
+    {
+        return 'SimpleTestEvent';
     }
 }
