a5eb6287f7024dbe46e14dc1553f2211e6f17366
andersonseidler <andersonqipoa@gmail.com>
2025-03-20 20:58:30 -0300

    chat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,6 +36,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsPort: 6001,
     forceTLS: true,  // Se estiver usando HTTPS, mantenha como true
 });
 
