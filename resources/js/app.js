59a3f41820a44f6fdf0f16a776ea1826ed8ca524
andersonseidler <andersonqipoa@gmail.com>
2025-03-21 08:11:31 -0300

    chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("Elemento #message-list encontrado:", messageList);
 
     if (messageList) {
-        window.Echo.channel('chat')
+        window.Echo.private('chat')
             .listen('.NewMessage', (event) => {  // Adicione o ponto antes do nome do evento
                 console.log('Nova mensagem recebida:', event);
                 if (event && event.content && event.sender_id && event.created_at) {
