ee4a008dbb75b97d6e0ab475226e07bdccacf055
andersonseidler <andersonqipoa@gmail.com>
2025-03-23 21:14:25 -0300

    chat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -56,12 +56,11 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-
+window.Pusher.logToConsole = true; 
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wssPort: 443,
     forceTLS: true,
     disableStats: true,
     enabledTransports: ['wss'],
