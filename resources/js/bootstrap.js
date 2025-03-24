32e7408354554b03cc1b7266138624ad704056bc
andersonseidler <andersonqipoa@gmail.com>
2025-03-23 21:33:23 -0300

    chat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -31,24 +31,27 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-window.Pusher.logToConsole = true; // Ativa os logs de Pusher no console
+window.Pusher.logToConsole = true; // Isso permite visualizar os logs do Pusher no console
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wssPort: 443,  // Não usar o protocolo wss em ambientes não SSL
+    wssPort: 443,  
     forceTLS: true,
     encrypted: true, 
-    enabledTransports: ['ws', 'wss', 'xhr_streaming', 'xhr_polling'], // <-- Adiciona suporte a polling
-
+    enabledTransports: ['ws', 'wss', 'xhr_streaming', 'xhr_polling'], // Suporte ao polling
 });
-
-console.log("Instância de Echo criada com sucesso!");
 
 window.Echo.connector.pusher.connection.bind('state_change', function(states) {
-    console.log("Estado da conexão Pusher:", states);
+    console.log('Pusher state changed:', states);
     if (states.current === 'connected') {
-        console.log("Conexão Pusher estabelecida com sucesso!");
+        console.log('Conexão Pusher estabelecida com sucesso!');
     }
 });
+
+window.Echo.channel('chat')
+    .listen('MessageSent', (event) => {
+        console.log('Mensagem recebida:', event.message);
+    });
+
 
