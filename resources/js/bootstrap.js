45e348f14724caac97e3c61bee8d074cd826f13a
andersonseidler <andersonqipoa@gmail.com>
2025-03-23 21:17:43 -0300

    chat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -27,44 +27,20 @@
 
 
 // PRODUCAO
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
-
-// window.Pusher = Pusher;
-// window.Pusher.logToConsole = true; // Ativa os logs de Pusher no console
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     wssPort: 443,  // Não usar o protocolo wss em ambientes não SSL
-//     forceTLS: true,
-//     encrypted: true, 
-//     enabledTransports: ['ws', 'wss', 'xhr_streaming', 'xhr_polling'], // <-- Adiciona suporte a polling
-
-// });
-
-// console.log("Instância de Echo criada com sucesso!");
-
-// window.Echo.connector.pusher.connection.bind('state_change', function(states) {
-//     console.log("Estado da conexão Pusher:", states);
-//     if (states.current === 'connected') {
-//         console.log("Conexão Pusher estabelecida com sucesso!");
-//     }
-// });
-
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-window.Pusher.logToConsole = true; 
+window.Pusher.logToConsole = true; // Ativa os logs de Pusher no console
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wssPort: 443,
+    wssPort: 443,  // Não usar o protocolo wss em ambientes não SSL
     forceTLS: true,
-    encrypted: true,
-    enabledTransports: ['wss'],
+    encrypted: true, 
+    enabledTransports: ['ws', 'wss', 'xhr_streaming', 'xhr_polling'], // <-- Adiciona suporte a polling
+
 });
 
 console.log("Instância de Echo criada com sucesso!");
@@ -73,7 +49,6 @@ window.Echo.connector.pusher.connection.bind('state_change', function(states) {
     console.log("Estado da conexão Pusher:", states);
     if (states.current === 'connected') {
         console.log("Conexão Pusher estabelecida com sucesso!");
-    } else if (states.current === 'failed') {
-        console.error("Falha na conexão com Pusher:", states);
     }
 });
+
