5bada4fbc3b9d1b32db6c3f114313362475500d8
andersonseidler <andersonqipoa@gmail.com>
2025-03-25 22:20:28 -0300

    chat

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,5 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'api/create-preference',
         'api/payment-updated',
+        '/login', // Exclui a verificação CSRF para essa rota
     ];
 }
