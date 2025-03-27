235aa95ec8144a9a2852b204c063215b5066419d
andersonseidler <andersonqipoa@gmail.com>
2025-03-27 20:30:11 -0300

    chat autenticado

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,17 +1,49 @@
 // DESENVOLVIMENTO
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
+
+// window.Pusher = Pusher;
+// window.Pusher.logToConsole = true;
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+//     wsHost: window.location.hostname,
+//     wsPort: 6001,
+//     wssPort: 6001,
+//     forceTLS: false,
+//     authEndpoint: '/broadcasting/auth',
+//     auth: {
+//     headers: {
+//         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+//         'Authorization': `Bearer ${localStorage.getItem('token')}` //DUVIDA ?
+//     },
+// },
+
+// });
+
+// console.log("Instância de Echo criada com sucesso!");
+
+// window.Echo.connector.pusher.connection.bind('state_change', function(states) {
+//     console.log("Estado da conexão Pusher:", states);
+//     if (states.current === 'connected') {
+//         console.log("Conexão Pusher estabelecida com sucesso!");
+//     }
+// });
+
+
+// PRODUCAO
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-window.Pusher.logToConsole = true;
+window.Pusher.logToConsole = true;  // Ativar para depuração, pode ser removido em produção
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
-    forceTLS: false,
+    encrypted: true,
+    forceTLS: true,
     authEndpoint: '/broadcasting/auth',
     auth: {
     headers: {
@@ -30,28 +62,5 @@ window.Echo.connector.pusher.connection.bind('state_change', function(states) {
         console.log("Conexão Pusher estabelecida com sucesso!");
     }
 });
-
-
-// PRODUCAO
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
-
-// window.Pusher = Pusher;
-// window.Pusher.logToConsole = true;  // Ativar para depuração, pode ser removido em produção
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     encrypted: true
-// });
-
-// console.log("Instância de Echo criada com sucesso!");
-
-// window.Echo.connector.pusher.connection.bind('state_change', function(states) {
-//     console.log("Estado da conexão Pusher:", states);
-//     if (states.current === 'connected') {
-//         console.log("Conexão Pusher estabelecida com sucesso!");
-//     }
-// });
 
 
