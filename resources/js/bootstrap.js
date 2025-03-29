fc913d5f7c888d83ff7b63c82559424c4a95251e
andersonseidler <andersonqipoa@gmail.com>
2025-03-29 15:44:33 -0300

    chat autenticado

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,37 @@
 // DESENVOLVIMENTO
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
+window.Pusher.logToConsole = true;
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    wssPort: 6001,
+    forceTLS: false,
+    encrypted: false,
+    authEndpoint: '/broadcasting/auth',
+    auth: {
+    headers: {
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+    },
+},
+
+});
+
+console.log("Instância de Echo criada com sucesso!");
+
+window.Echo.connector.pusher.connection.bind('state_change', function(states) {
+    console.log("Estado da conexão Pusher:", states);
+    if (states.current === 'connected') {
+        console.log("Conexão Pusher estabelecida com sucesso!");
+    }
+});
+
+// PRODUCAO
 // import Echo from 'laravel-echo';
 // import Pusher from 'pusher-js';
 
@@ -8,53 +41,18 @@
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
 //     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: window.location.hostname,
-//     wsPort: 6001,
-//     wssPort: 6001,
-//     forceTLS: false,
-//     encrypted: false,
+//     wsHost: 'proconline.com.br',
+//     wsPort: 443,  // Altere para 443
+//     wssPort: 443, // Altere para 443
+//     forceTLS: true,
+//     encrypted: true,
 //     authEndpoint: '/broadcasting/auth',
 //     auth: {
-//     headers: {
-//         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+//         headers: {
+//             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+//         },
 //     },
-// },
-
 // });
-
-// console.log("Instância de Echo criada com sucesso!");
-
-// window.Echo.connector.pusher.connection.bind('state_change', function(states) {
-//     console.log("Estado da conexão Pusher:", states);
-//     if (states.current === 'connected') {
-//         console.log("Conexão Pusher estabelecida com sucesso!");
-//     }
-// });
-
-// PRODUCAO
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-
-window.Pusher = Pusher;
-window.Pusher.logToConsole = true;
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,  // Variável de ambiente para a chave do Pusher
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: 'proconline.com.br',  // Servidor do WebSocket
-    wsPort: 443,  // Porta 443 para HTTPS
-    wssPort: 443, // Porta 443 para WebSocket seguro (wss)
-    forceTLS: true,  // Forçar uso de TLS
-    encrypted: true,  // Encriptação habilitada
-    authEndpoint: '/broadcasting/auth',  // Endpoint de autenticação
-    auth: {
-        headers: {
-            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
-        },
-    },
-});
-
 
 
 
