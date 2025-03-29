0882d449defb854163e22df48c6e6a4d24c2caf7
andersonseidler <andersonqipoa@gmail.com>
2025-03-29 17:16:33 -0300

    chat autenticado

--- routes/channels.php
+++ routes/channels.php
@@ -6,18 +6,7 @@ use Illuminate\Support\Facades\Log;
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 
-
-Broadcast::channel('chat', function () {
-    \Log::info('Tentativa de inscrição no canal "chat"');
-    return true;  // Para permitir inscrição sem autenticação
+Broadcast::channel('chat', function ($user) {
+    \Log::info('Tentativa de inscrição no canal "chat"', ['user' => $user]);
+    return $user !== null;  // Permite inscrição apenas para usuários autenticados
 });
-
-
-
-// Broadcast::channel('private-chat.{chatId}', function ($user, $chatId) {
-//     return true; // Permitir acesso a todos os usuários para teste
-// });
-
-// Route::post('/broadcasting/auth', function (Illuminate\Http\Request $request) {
-//     return Broadcast::auth($request);
-// })->middleware('auth');  // Verifique se o middleware 'auth' está permitindo o acesso
