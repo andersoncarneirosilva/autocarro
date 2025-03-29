14b07b3748e29a6cbf1a5aa98be181aafd86508e
andersonseidler <andersonqipoa@gmail.com>
2025-03-29 15:48:04 -0300

    chat autenticado

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,37 @@
 // DESENVOLVIMENTO
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
+
+// window.Pusher = Pusher;
+// window.Pusher.logToConsole = true;
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+//     wsHost: window.location.hostname,
+//     wsPort: 6001,
+//     wssPort: 6001,
+//     forceTLS: false,
+//     encrypted: false,
+//     authEndpoint: '/broadcasting/auth',
+//     auth: {
+//     headers: {
+//         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+//     },
+// },
+
+// });
+
+// console.log("Instância de Echo criada com sucesso!");
+
+// window.Echo.connector.pusher.connection.bind('state_change', function(states) {
+//     console.log("Estado da conexão Pusher:", states);
+//     if (states.current === 'connected') {
+//         console.log("Conexão Pusher estabelecida com sucesso!");
+//     }
+// });
+
+// PRODUCAO
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
@@ -9,8 +42,8 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
+    wsPort: 443,
+    wssPort: 443,
     forceTLS: false,
     encrypted: false,
     authEndpoint: '/broadcasting/auth',
@@ -21,38 +54,6 @@ window.Echo = new Echo({
 },
 
 });
-
-console.log("Instância de Echo criada com sucesso!");
-
-window.Echo.connector.pusher.connection.bind('state_change', function(states) {
-    console.log("Estado da conexão Pusher:", states);
-    if (states.current === 'connected') {
-        console.log("Conexão Pusher estabelecida com sucesso!");
-    }
-});
-
-// PRODUCAO
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
-
-// window.Pusher = Pusher;
-// window.Pusher.logToConsole = true;
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: 'proconline.com.br',
-//     wsPort: 443,  // Altere para 443
-//     wssPort: 443, // Altere para 443
-//     forceTLS: true,
-//     encrypted: true,
-//     authEndpoint: '/broadcasting/auth',
-//     auth: {
-//         headers: {
-//             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
-//         },
-//     },
-// });
 
 
 
