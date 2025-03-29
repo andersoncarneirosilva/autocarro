6bee539bd448925dde2ee9931851572666e455b5
andersonseidler <andersonqipoa@gmail.com>
2025-03-29 14:33:16 -0300

    chat publico

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -41,7 +41,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: window.location.hostname,
+    //wsHost: window.location.hostname,
     wsHost: 'proconline.com.br',
     wsPort: 6001,
     wssPort: 6001,
