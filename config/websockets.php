3f81a8a3533384de23b4a15db517e20131ba7cf9
andersonseidler <andersonqipoa@gmail.com>
2025-03-30 09:50:12 -0300

    chat teste

--- config/websockets.php
+++ config/websockets.php
@@ -108,16 +108,16 @@ return [
      * Define the optional SSL context for your WebSocket connections.
      * You can see all available options at: http://php.net/manual/en/context.ssl.php
      */
+    // 'ssl' => [
+    //     'local_cert' => null,
+    //     'local_pk' => null,
+    //     'passphrase' => null,
+    //     'verify_peer' => false,
+    // ],
     'ssl' => [
-        'local_cert' => null,
-        'local_pk' => null,
-        'passphrase' => null,
-        'verify_peer' => false,
-    ],
-//     'ssl' => [
-//     'local_cert' => '/etc/letsencrypt/live/proconline.com.br/fullchain.pem',
-//     'local_pk' => '/etc/letsencrypt/live/proconline.com.br/privkey.pem',
-// ],
+    'local_cert' => '/etc/letsencrypt/live/proconline.com.br/fullchain.pem',
+    'local_pk' => '/etc/letsencrypt/live/proconline.com.br/privkey.pem',
+],
 
     'replication' => [
             'mode' => 'single',
