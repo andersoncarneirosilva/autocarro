fd64116aed2e1520abfa6d961ab76e4a164791db
andersonseidler <andersonqipoa@gmail.com>
2025-03-30 10:33:15 -0300

    chat teste

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,18 +28,21 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
+window.Pusher.logToConsole = true;  // Desabilite isso em produção
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
     encrypted: true,
+    authEndpoint: '/broadcasting/auth',
     auth: {
         headers: {
-            Authorization: 'Bearer ' + window.Laravel.user.token,  // se você estiver usando autenticação
-        }
-    }
+            'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]')?.content || '',
+        },
+    },
+    
 });
 
 
