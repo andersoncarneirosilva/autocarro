6cf1f8d5de11f487a87187df0aa505800aa90f46
andersonseidler <andersonqipoa@gmail.com>
2026-01-19 14:46:19 -0300

    cadastro revenda

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,14 +11,25 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        User::create([
-            'name' => 'Anderson',
-            'email' => 'andersonqipoa@gmail.com',
-            'telefone' => '(51)9.99137.7276',
-            'nivel_acesso' => 'Administrador',
-            'password' => bcrypt('12345678'),
-            'status' => 'Ativo',
-        ]);
-    }
+{
+    // Usuário Anderson
+    User::create([
+        'name' => 'Anderson',
+        'email' => 'andersonqipoa@gmail.com',
+        'telefone' => '(51)9.99137.7276',
+        'nivel_acesso' => 'Administrador',
+        'password' => bcrypt('12345678'),
+        'status' => 'Ativo',
+    ]);
+
+    // Usuário Pedro
+    User::create([
+        'name' => 'Pedro',
+        'email' => 'pedro@gmail.com',
+        'telefone' => '(51)9.9999.9999',
+        'nivel_acesso' => 'Usuário',
+        'password' => bcrypt('12345678'),
+        'status' => 'Ativo',
+    ]);
+}
 }
