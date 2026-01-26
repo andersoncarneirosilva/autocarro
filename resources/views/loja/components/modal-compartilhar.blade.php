9ca516cdad692233320d4a8d775589672fbb326f
andersonseidler <andersonqipoa@gmail.com>
2026-01-25 22:10:26 -0300

    update cadastro manual

--- resources/views/loja/components/modal-compartilhar.blade.php
+++ resources/views/loja/components/modal-compartilhar.blade.php
@@ -4,7 +4,7 @@
             <div class="modal-header border-0 pb-0">
                 <div class="ps-2 pt-2">
                     <h5 class="fw-bold mb-1">Compartilhar Veículo</h5>
-                    <p class="text-muted small mb-0">{{ $veiculo->marca_real }} {{ $veiculo->modelo_real }}</p>
+                    <p class="text-muted small mb-0">Compartilhe nas redes</p>
                 </div>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
@@ -34,14 +34,6 @@
                             <i class="bi bi-facebook text-white fs-5"></i>
                         </div>
                         <span class="fw-medium">Facebook</span>
-                    </a>
-
-                    <a href="javascript:void(0)" onclick="socialPopup('https://twitter.com/intent/tweet?url={{ urlencode($urlAtual) }}&text={{ urlencode($textoShare) }}')"
-                       class="list-group-item list-group-item-action d-flex align-items-center border-0 py-3">
-                        <div class="rounded-circle d-flex align-items-center justify-content-center me-3" style="width: 40px; height: 40px; background-color: #000;">
-                            <i class="bi bi-twitter-x text-white fs-5"></i>
-                        </div>
-                        <span class="fw-medium">X (Twitter)</span>
                     </a>
 
                     <button onclick="copyToClipboard()" class="list-group-item list-group-item-action d-flex align-items-center border-0 py-3">
