3d98b59b846c3b613d0ee359b91e745538c833e1
andersonseidler <andersonqipoa@gmail.com>
2026-02-02 23:34:07 -0300

    login app

--- app/Http/Controllers/Api/ApiDashController.php
+++ app/Http/Controllers/Api/ApiDashController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Resources\Api\DashboardResource;
+use App\Http\Resources\DashboardResource;
 use App\Http\Controllers\Controller;
 use App\Models\Cliente;
 use App\Models\DashModel;
